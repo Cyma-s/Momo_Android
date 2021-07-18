f396cc7ba56a2616a4162cb988f069e0d720e42d
jaehoon0 <choco730522@gmail.com>
2021-07-18 22:56:46 +0900

    create(Splash)

--- app/src/main/java/com/example/momo/MainActivity.java
+++ app/src/main/java/com/example/momo/MainActivity.java
@@ -3,9 +3,9 @@ package com.example.momo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
