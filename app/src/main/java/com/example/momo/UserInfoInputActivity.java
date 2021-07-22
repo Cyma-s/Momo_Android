f57a524c8e8ca34291785463e6db4f09d83c16d3
Eunsol Kim <ssol0319@naver.com>
2021-07-23 01:29:31 +0900

    edit ( UserInfoInputActivity, LoginActivity ) : 사용자의 카카오톡 프로필 사진 가져올 수 있음

--- app/src/main/java/com/example/momo/UserInfoInputActivity.java
+++ app/src/main/java/com/example/momo/UserInfoInputActivity.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.kakao.sdk.user.UserApiClient;
+
+import java.util.Base64;
 
 import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
@@ -24,8 +31,14 @@ public class UserInfoInputActivity extends AppCompatActivity {
 
         TextView userName = findViewById(R.id.nickname_edittext);
         Button logoutButton = findViewById(R.id.logout_btn);
+        ImageView imageView = findViewById(R.id.userProfileImage);
+
+        String userImageString = sharedPreferences.getString("userImage", "");
+        Log.i("userInfo", userImageString);
+        Bitmap userImage = StringToBitmap(userImageString);
         String id = sharedPreferences.getString("userNickName", "");
         userName.setText(id);
+        imageView.setImageBitmap(userImage);
 
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,5 +55,16 @@ public class UserInfoInputActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public Bitmap StringToBitmap(String imgString){
+        try {
+            byte[] encodeByte = Base64.getDecoder().decode(imgString);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
