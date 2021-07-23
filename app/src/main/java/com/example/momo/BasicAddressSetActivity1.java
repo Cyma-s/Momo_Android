d65fb2d72334b76af622540304ed8d9e56b1f95b
jaehoon0 <choco730522@gmail.com>
2021-07-24 04:07:53 +0900

    edit(addressSearch)

---
+++ app/src/main/java/com/example/momo/BasicAddressSetActivity1.java
@@ -0,0 +1,14 @@
+package com.example.momo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BasicAddressSetActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_address_set1);
+    }
+}
