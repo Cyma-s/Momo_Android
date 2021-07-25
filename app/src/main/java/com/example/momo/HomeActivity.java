fafc1a22eb2d2157386f7b02bb2067e390fdb9a8
Eunsol Kim <ssol0319@naver.com>
2021-07-25 15:00:12 +0900

    edit ( MeetingAdapter, Meeting ) : 리사이클러뷰 어댑터, 아이템 추가 (서버 코드 제외)

--- app/src/main/java/com/example/momo/HomeActivity.java
+++ app/src/main/java/com/example/momo/HomeActivity.java
@@ -1,14 +1,34 @@
 package com.example.momo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+import java.util.ArrayList;
+
 public class HomeActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    private ArrayList<Meeting> list = new ArrayList<>();
+    private LinearLayoutManager layoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        recyclerView = findViewById(R.id.meeting_recyclerview);
     }
+
+    public void getMeetings() {
+        RequestQueue queue = Volley.newRequestQueue(HomeActivity.this);
+        String url = getString(R.string.url);
+    }
+
+
 }
