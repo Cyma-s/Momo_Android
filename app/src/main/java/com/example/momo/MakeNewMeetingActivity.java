0cd628c35395fb0bdbe2b6cc3bb776d9a7ecb925
Eunsol Kim <ssol0319@naver.com>
2021-07-26 15:34:25 +0900

    edit ( MakeNewMeetingActivity ) : xml 제작중

--- app/src/main/java/com/example/momo/MakeNewMeetingActivity.java
+++ app/src/main/java/com/example/momo/MakeNewMeetingActivity.java
@@ -3,12 +3,16 @@ package com.example.momo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.NumberPicker;
 
 public class MakeNewMeetingActivity extends AppCompatActivity {
+    NumberPicker numberPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_new_meeting);
+
+        numberPicker = findViewById(R.id.)
     }
 }
