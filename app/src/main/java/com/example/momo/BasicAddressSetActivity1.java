8491660c3d40a3942135a339bc03afa3ecf9a000
jaehoon0 <choco730522@gmail.com>
2021-07-26 15:26:19 +0900

    edit(addressBasic)

--- app/src/main/java/com/example/momo/BasicAddressSetActivity1.java
+++ app/src/main/java/com/example/momo/BasicAddressSetActivity1.java
@@ -2,13 +2,45 @@ package com.example.momo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class BasicAddressSetActivity1 extends AppCompatActivity {
+    // 주소 요청코드 상수 requestCode
+    private static final int SEARCH_ADDRESS_ACTIVITY = 10000;
+    private EditText address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        address = findViewById(R.id.address);
+        address.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), AddressApiActivity.class);
+                //화면전환 애니메이션 없앰
+                overridePendingTransition(0, 0);
+                startActivityForResult(i, SEARCH_ADDRESS_ACTIVITY);
+            }
+        });
         setContentView(R.layout.activity_basic_address_set1);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        switch (requestCode) {
+            case SEARCH_ADDRESS_ACTIVITY:
+                if (resultCode == RESULT_OK) {
+                    String data = intent.getExtras().getString("data");
+                    if (data != null) {
+                        address.setText(data);
+                    }
+                }
+                break;
+        }
     }
 }
