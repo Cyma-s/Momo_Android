07f1d4331bcb65eccb824df5d9848f7164d182cd
Eunsol Kim <ssol0319@naver.com>
2021-07-29 01:02:08 +0900

    edit ( RecommendOrNot ) : RecommendDate와 연결

--- app/src/main/java/com/example/momo/RecommendOrNot.java
+++ app/src/main/java/com/example/momo/RecommendOrNot.java
@@ -1,10 +1,13 @@
 package com.example.momo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 
 public class RecommendOrNot extends AppCompatActivity {
 
@@ -13,6 +16,12 @@ public class RecommendOrNot extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommend_or_not);
 
+        RadioGroup meetingGroup = findViewById(R.id.meetingdayrecommend);
+        int selectedDateId = meetingGroup.getCheckedRadioButtonId();
+        if(selectedDateId == R.id.day_yes) {
+            Intent intent = new Intent(RecommendOrNot.this, RecommendDate.class);
+            startActivity(intent);
+        }
 
         Button nextButton = findViewById(R.id.newMeetingRecNext);
         // 다음 버튼 클릭 시
