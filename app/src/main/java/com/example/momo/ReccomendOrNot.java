5f827ce5fd2d1e03ad0c85293cad6332545bb65e
Eunsol Kim <ssol0319@naver.com>
2021-07-28 20:19:04 +0900

    edit ( activity_reccomend_or_not.xml ) : 추천 여부 xml 만들기

---
+++ app/src/main/java/com/example/momo/ReccomendOrNot.java
@@ -0,0 +1,14 @@
+package com.example.momo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReccomendOrNot extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reccomend_or_not);
+    }
+}
