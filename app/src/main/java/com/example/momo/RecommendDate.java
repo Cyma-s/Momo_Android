79bc24a88a9560ed709c253c6a296fadba52f7ef
Eunsol Kim <ssol0319@naver.com>
2021-07-31 02:52:29 +0900

    edit ( RecommendDate ) : NumberPicker array 추가

--- app/src/main/java/com/example/momo/RecommendDate.java
+++ app/src/main/java/com/example/momo/RecommendDate.java
@@ -3,7 +3,12 @@ package com.example.momo;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.OrientationHelper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.NumberPicker;
 import android.widget.Toast;
 
 import com.applikeysolutions.cosmocalendar.dialog.CalendarDialog;
@@ -24,7 +29,9 @@ import java.util.List;
 
 public class RecommendDate extends AppCompatActivity{
     private CalendarView calendarView;
-
+    ArrayList<String> meetingTimeStart = new ArrayList<>();
+    ArrayList<String> meetingTimeEnd = new ArrayList<>();
+    private NumberPicker start, end;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,8 @@ public class RecommendDate extends AppCompatActivity{
         setContentView(R.layout.activity_recommend_date);
 
         initViews();
+        setNumberPicker();
+        Button nextButton = findViewById(R.id.recommendDayNext);
 
         calendarView.setSelectionManager(new MultipleSelectionManager(new OnDaySelectedListener() {
             @Override
@@ -39,12 +48,22 @@ public class RecommendDate extends AppCompatActivity{
                 selectDates();
             }
         }));
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecommendDate.this, UnableTimeInput.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void initViews() {
         calendarView = (CalendarView) findViewById(R.id.makeMeetingCalender);
         calendarView.setCalendarOrientation(OrientationHelper.HORIZONTAL);
         calendarView.setSelectionType(SelectionType.MULTIPLE);
+        start = findViewById(R.id.startTime);
+        end = findViewById(R.id.endTime);
     }
 
     private void selectDates() {
@@ -59,5 +78,29 @@ public class RecommendDate extends AppCompatActivity{
             result += year + "년 " + month + "월 " + day + "일 " + week + "요일 \n";
         }
         Toast.makeText(RecommendDate.this, result, Toast.LENGTH_LONG).show();
+    }
+
+    private void setNumberPicker() {
+        for(int i = 0; i<24; i++) {
+            meetingTimeStart.add(i + "시");
+            meetingTimeEnd.add(i + "시");
+        }
+        String[] startArr = meetingTimeStart.toArray(new String[0]);
+        String[] endArr = meetingTimeEnd.toArray(new String[0]);
+        start.setDisplayedValues(startArr);
+        end.setDisplayedValues(endArr);
+
+        start.setWrapSelectorWheel(false);
+        start.setDescendantFocusability(
+                ViewGroup.FOCUS_BLOCK_DESCENDANTS
+        );
+        start.setMinValue(0);
+        start.setMaxValue(23);
+        end.setWrapSelectorWheel(false);
+        end.setDescendantFocusability(
+                ViewGroup.FOCUS_BLOCK_DESCENDANTS
+        );
+        end.setMinValue(0);
+        end.setMaxValue(23);
     }
 }
