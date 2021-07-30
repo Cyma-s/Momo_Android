64270f84bd0666aa37268973234e8b3417292b65
Eunsol Kim <ssol0319@naver.com>
2021-07-31 02:37:44 +0900

    edit (recommend 관련 xml) : xml 수정 add ( activity_unable_time_input.xml ) : xml 구성 (스피너? 제외)

---
+++ app/src/main/java/com/example/momo/UnableTimeInput.java
@@ -0,0 +1,14 @@
+package com.example.momo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UnableTimeInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unable_time_input);
+    }
+}
