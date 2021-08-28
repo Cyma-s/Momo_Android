17f9a36ced6b78101ded6e41de2ad59ec34e6eea
Eunsol Kim <ssol0319@naver.com>
2021-08-28 21:46:58 +0900

    edit ( LoginActivity ) : 로그인 안 되어 있을 때 링크 클릭하면 로그인 후 다시 돌아가도록 (이슈 해결중)

--- app/src/main/java/com/example/momo/MakeNewMeetingActivity.java
+++ app/src/main/java/com/example/momo/MakeNewMeetingActivity.java
@@ -32,7 +32,7 @@ public class MakeNewMeetingActivity extends AppCompatActivity {
         nextButton.setOnClickListener(new View.OnClickListener() { // 다음 버튼 클릭 시 추천 여부 activity로 이동
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MakeNewMeetingActivity.this, RecommendOrNot.class);
+                Intent intent = new Intent(MakeNewMeetingActivity.this, RecommendDate.class);
                 startActivity(intent);
             }
         });
